023b6f123ffe1a8cb18675b8f50c6c36ba689207
Dhanasri Bapatla <dhanasri.bapatla@valuelabs.net>
2012-12-17 12:47:27 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeApp::Application.config.secret_token = 'ff7a3975a87b1631675ddd91c9a0328567778a4f3b4cf49aca1e2245031a3080d32d05cad52451584b6342966ea5cc213cc784be11010f097b9e282ba70e23f6'
